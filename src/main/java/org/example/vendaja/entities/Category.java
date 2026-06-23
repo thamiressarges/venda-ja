84126ee5b3d340c19290b0534ab6c318743ab03b
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-23 13:01:13 -0400

    feat: mapeia entidade Category com anotações JPA

--- src/main/java/org/example/vendaja/entities/Category.java
+++ src/main/java/org/example/vendaja/entities/Category.java
@@ -1,11 +1,17 @@
 package org.example.vendaja.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
