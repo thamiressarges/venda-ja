5735a9789c341e119eabd0f5636cbbbcc4a98697
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-23 12:29:53 -0400

    First commit

---
+++ src/test/java/org/example/vendaja/VendaJaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vendaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaJaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
