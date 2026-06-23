0143f67a43e6d0eb15cfc2ee01de68e20f81f7ac
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-23 15:06:57 -0400

    refactor: adiciona transação somente leitura na listagem de categorias

--- src/main/java/org/example/vendaja/resources/CategoryResource.java
+++ src/main/java/org/example/vendaja/resources/CategoryResource.java
@@ -1,9 +1,9 @@
 package org.example.vendaja.resources;
-
 import org.example.vendaja.entities.Category;
 import org.example.vendaja.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +18,9 @@ public class CategoryResource {
     @Autowired
     private CategoryService service;
 
+    @Transactional(readOnly = true)
     @GetMapping
-    public ResponseEntity<List<Category>> fincAll(){
+    public ResponseEntity<List<Category>> findAll(){
         List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
