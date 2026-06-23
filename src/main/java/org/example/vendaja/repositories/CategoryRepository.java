339c4c63b35763251ae35f771315350709c0c79e
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-23 13:04:54 -0400

    feat: adiciona repositório de categorias

---
+++ src/main/java/org/example/vendaja/repositories/CategoryRepository.java
@@ -0,0 +1,6 @@
+package org.example.vendaja.repositories;
+import org.example.vendaja.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
