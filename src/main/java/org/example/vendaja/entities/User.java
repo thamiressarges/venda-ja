80d815c313e6ed306db3886222b9aaa8938e3921
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-27 10:44:28 -0400

    feat: adiciona modelo User com relacionamento de roles

---
+++ src/main/java/org/example/vendaja/entities/User.java
@@ -0,0 +1,75 @@
+package org.example.vendaja.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class User implements Serializable {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String password;
+
+    private Set<Role> roles = new HashSet<>();
+
+    public User(){
+
+    }
+
+    public User(Long id, String firstName, String lastName, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
